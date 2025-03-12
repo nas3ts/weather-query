6b4a2140213436dac36f0eaf0132f9ee4bbb5f1f
Nas3ts <nkisowile@gmail.com>
2025-03-12 22:09:53 +0300

    fix: add riht input validation for city suggestions

--- scripts.js
+++ scripts.js
@@ -42,7 +42,7 @@
         //function called by oninput event
             function showCities() {
 
-                if (validateSearch(city)) { // input goes through validation
+                if (!city.checkValidity()) { // input goes through validation
                     const currentInput = city.value.trim(); // input value taken and all white spaces removed
                     const cityOptions = document.getElementById("city-options");
                     cityOptions.innerHTML = "City Options"; // clearing previous options
